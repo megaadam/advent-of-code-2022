792d6bf23dad5b2fd5e1fb5e67bf979b278479b4
adam <adam.horvath@edgeware.tv>
2022-12-17 22:47:51 +0100

    wip

--- src/day17/one.py
+++ src/day17/one.py
@@ -3,8 +3,6 @@ sys.path.append('../util')
 import util
 
 
-raw_moves = '>>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>'
-
 raw_shapes = [
     ['####',],
 
@@ -50,15 +48,14 @@ shapes = get_shapes()
 ix = 0
 def next_shape():
     global ix
-    shape = shapes[ix% len(shapes)]
+    shape = shapes[ix % len(shapes)]
+    ix+=1
     
     return shape
 
 i2 = 0
 def next_move():
     global i2
-    if i2 == len(raw_moves):
-        return 0
 
     if raw_moves[i2 % len(raw_moves)] == '<':
         i2 +=1
@@ -95,7 +92,7 @@ def can_fall(pit, shape, shape_x, shape_y):
 
 def can_move(pit, shape, move, shape_x, shape_y):
     width = len(pit[0])
-    if(shape_x+move > 0 or shape_x+move>7):
+    if shape_x+move < 0 or shape_x+len(shape[0])+move>7:
         return False
 
     for iy, shape_line in enumerate(shape):
@@ -108,14 +105,18 @@ def can_move(pit, shape, move, shape_x, shape_y):
 def extend_pit(pit, shape, headroom=3, width=7):
     if len(pit) == 0:
         init_pit()
-        return
+        return pit
     
     for iy,line in enumerate(pit):
         if sum(line):
-            add = max((len(shape)-iy+headroom), 0)
-            for _ in range(add):
+            diff = (len(shape)-iy+headroom)
+            if diff < 0:
+                pit=pit[-diff:]
+                return pit
+
+            for _ in range(diff):
                 pit.insert(0, get_line())
-            return
+            return pit
 
 def calc_height(pit):
     for iy,line in enumerate(pit):
@@ -134,6 +135,24 @@ def merge(pit, shape, shape_x, shape_y):
             pit[shape_y+iy][shape_x+ix] = cell
 
 
+def print_pit(pit, shape=[], shape_y=0):
+    print()
+    for iy, line in enumerate(pit):
+        for ix, pos in enumerate(line):
+            if shape and shape_y == 0 and ix > 1 and ix-2<len(shape[0]) and iy<(len(shape)):
+                if shape[iy][ix-2]:
+                    print('@', end='')
+                else:
+                    print('.', end='')
+            
+                continue
+
+            if pos:
+                print('#', end='')
+            else:
+                print('.', end='')
+        print()
+
 
 def run_moves(rox, wdith=7):
     pit = []
@@ -144,23 +163,32 @@ def run_moves(rox, wdith=7):
     shape_y = 0
 
     for counter in range(rox):
-        move = next_move()
+        while(True):
+            move = next_move()
 
-        if(can_move(pit, shape, move, shape_x, shape_y)):
-            shape_x += move
+            if(can_move(pit, shape, move, shape_x, shape_y)):
+                shape_x += move
 
-        if can_fall(pit, shape, shape_x, shape_y):
-            shape_y += 1
-        else:
-            # came to halt
-            # merge
-            merge(pit, shape, shape_x, shape_y)
+            if can_fall(pit, shape, shape_x, shape_y):
+                shape_y += 1
+            else:
+                # came to halt
+                # merge
+                merge(pit, shape, shape_x, shape_y)
 
-            # next
-            shape = next_shape()
-            shape_x = 2
-            shape_y = 2
-            extend_pit(pit, shape)
+                # next
+                shape = next_shape()
+                shape_x = 2
+                shape_y = 0
+                ha = calc_height(pit)
+                pit = extend_pit(pit, shape)
+                hb = calc_height(pit)
+                assert ha==hb
+
+
+                #print_pit(pit, shape, 0)
+
+                break
 
     h = calc_height(pit)
     print("Height: ", h)
@@ -168,7 +196,15 @@ def run_moves(rox, wdith=7):
 
 
 def test():
-
+    print("raw moves", len(raw_moves))
     run_moves(2022)
 
+
+
+#f = open('test_input', 'r')
+f = open('input', 'r')
+lines = f.readlines()
+raw_moves = lines[0].rstrip()
 test()
+
+print('ix', ix, 'i2',i2)
