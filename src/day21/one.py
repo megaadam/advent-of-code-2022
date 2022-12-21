9b8b85b1f397eb751810a1ba3758266f1ccd30a2
adam <adam.horvath@edgeware.tv>
2022-12-21 10:17:36 +0100

    21.1 pass test

--- src/day21/one.py
+++ src/day21/one.py
@@ -2,8 +2,9 @@ import sys
 sys.path.append('../util')
 import util
 
+monkeys = {}
 class Monkey:
-    __slots__ = ['name', 'op', 'pair_names', 'pair', 'val']
+    __slots__ = ['name', 'op', 'pair_names', 'v1', 'v2', 'val']
 
     def __init__(self, name, op=None, v1=None, v2=None, val=None):
         self.name = name
@@ -12,9 +13,36 @@ class Monkey:
             self.op = op
             self.val = None
         else:
-            self.val = int(v1)
+            self.val = int(val)
             self.pair_names = None
             self.op = None
+
+        v1, v2 = None, None
+
+    def eval(self):
+        global monkeys
+        if(self.val):
+            return self.val
+
+        else:
+            v1 = monkeys[self.pair_names[0]]
+            v2 = monkeys[self.pair_names[1]]
+
+            if self.op == '+':
+                return v1.eval() + v2.eval()
+
+            if self.op == '-':
+                return v1.eval() - v2.eval()
+
+            if self.op == '*':
+                return v1.eval() * v2.eval()
+
+            if self.op == '/':
+                return v1.eval() / v2.eval()
+
+            print(self.name, self.v1, self.op, self.v2)
+            assert False
+
 
 
 def get_monkeys(lines):
@@ -24,8 +52,8 @@ def get_monkeys(lines):
         halves = line.split(':')
         assert len(halves) == 2
 
-        name = halves[0].rstrip()
-        h2 = halves[1]
+        name = halves[0].strip()
+        h2 = halves[1].strip()
 
         val, v1, v2 = None, None, None
         for op in ['+', '-', '*', '/']:
@@ -33,20 +61,19 @@ def get_monkeys(lines):
                 vs = h2.split(op)
                 assert len(halves) == 2
 
-                v1 = vs[0]
-                v2 = vs[1]
+                v1 = vs[0].strip()
+                v2 = vs[1].strip()
                 break
 
 
         if v2 != None:
             monkey = Monkey(name, op, v1, v2)
         else:
-            monkey = Monkey(name, val=v1)
+            monkey = Monkey(name, val=h2)
 
-        monkeys[name] = name
+        monkeys[name] = monkey
 
     return monkeys
-
 
 
 
@@ -57,5 +84,7 @@ lines = util.readlinesf('test_input')
 
 monkeys = get_monkeys(lines)
 print(monkeys)
+
+print(int(monkeys['root'].eval()))
 
 
