ee8a4b8e247a15aabecb7e538d315ee8a9dd9f93
adam <adam.horvath@edgeware.tv>
2022-12-23 18:54:22 +0100

    22.1 loading done

--- src/day22/one.py
+++ src/day22/one.py
@@ -4,19 +4,56 @@ import util
 
 from enum import Enum
 
+# https://adventofcode.com/2022/day/22
+
 lines = []
+grid = []
 
-Pos = Enum('Pos', ['NONE', 'OK', 'BLOCK'])
+Cell = Enum('Cell', ['NONE', 'OK', 'BLOCK'])
 
-def charpos(c):
+L = "L"
+R = "R"
+
+class Nav():
+    __slots__ = ['x', 'y', 'dx', 'dy']
+
+    def __init__(self):
+        self.dx = 1
+        self.dy = 0
+
+        for y, grid_line in enumerate(grid):
+            for x, cell in enumerate(grid_line):
+                if cell == Cell.OK:
+                    self.x = x
+                    self.y = y
+                    return
+
+        assert False, "WTF 1"
+
+
+def charcell(c):
     if c == ' ':
-        return Pos.NONE
+        return Cell.NONE
     if c == '.':
-        return Pos.OK
+        return Cell.OK
     if c == '#':
-        return Pos.BLOCK
+        return Cell.BLOCK
 
     assert False, 'WTF'
+
+def get_moves(moveline):
+    num = ""
+    moves = []
+    for m in moveline:
+        try:
+            val = int(num + m)
+            num += m
+        except ValueError:
+            move = (val, m)
+            moves.append(move)
+            num = ""
+
+    return moves
 
 def get_gridmoves(lines):
     lens = [len(l) for l in lines]
@@ -26,15 +63,16 @@ def get_gridmoves(lines):
     for line in lines[0:-2]:
         gridline = []
         for ix in range(maxlen):
-            gridline.append(charpos(line[ix]) if ix < len(line) else Pos.NONE)
+            gridline.append(charcell(line[ix]) if ix < len(line) else Cell.NONE)
         grid.append(gridline)
 
-    moves = lines[-1]
+    moves = get_moves(lines[-1])
 
     return grid, moves
 
 
 def test():
+    global grid
     grid, moves = get_gridmoves(lines)
     pass
 
