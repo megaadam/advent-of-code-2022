37b0d4e96d60bdc3ffaf1b28e486db9482a7f84d
adam <adam.horvath@edgeware.tv>
2022-12-27 00:37:48 +0100

    wip

--- src/day22/one.py
+++ src/day22/one.py
@@ -26,7 +26,6 @@ dirval = {
     (0, -1): 3,
 }
 
-
 def turn_move(x, y, turn):
     right, left = turn == "R", turn == "L"
 
@@ -39,14 +38,14 @@ def turn_move(x, y, turn):
 
     return x, y
 
-
 class Nav():
-    __slots__ = ['x', 'y', 'dx', 'dy', 'move_len']
+    __slots__ = ['x', 'y', 'dx', 'dy', 'move_len', 'grid']
 
-    def __init__(self):
+    def __init__(self, grid):
         self.dx = 1
         self.dy = 0
         self.move_len = 0
+        self.grid = grid
 
         for y, grid_line in enumerate(grid):
             for x, cell in enumerate(grid_line):
@@ -57,20 +56,64 @@ class Nav():
 
         assert False, "WTF 1"
 
+    def rowlen_minmax(self):
+        xmin = min(self.grid[self.y].index(Cell.OK), self.grid[self.y].index(Cell.BLOCK))
+        rev = self.grid[self.y][::-1]
+        xmax = len(self.grid[self.y]) - min(rev.index(Cell.OK), rev.index(Cell.BLOCK))
+
+        return xmax - xmin +1, xmin, xmax
+
+    def collen_minmax(self):
+        x = self.x
+        y = self.y
+
+        while True:
+            ymax = y
+            y += 1
+            if y > len(self.grid) -1 or self.grid[y][x] == Cell.NONE:
+                break
+
+        y = self.y
+        while True:
+            ymin = y
+            y -= 1
+            if y <0 or self.grid[y][x] == Cell.NONE:
+                break
+
+        return ymax - ymin + 1, ymin, ymax
+
+    def run_move(self, move):
+        if move.dy == 0:
+            rowlen, xmin, xmax = self.rowlen_minmax()
+            d = move.dist % rowlen
+        else:
+            collen, ymin, ymax = self.collen_minmax()
+            d = move.dist % collen
+
+        for _ in range(d):
+                if move.dy == 0:
+                    xtry = ((self.x + move.dx) % rowlen) + xmin
+                    ytry = self.y
+                else:
+                    xtry = self.x
+                    ytry = ((self.y + move.dy) % collen) + ymin
+
+                if self.grid[ytry][xtry] == Cell.BLOCK:
+                    break
+
+                self.x = xtry
+                self.y = ytry
+
+    def run(self, moves):
+        for move in moves:
+            self.run_move(move)
+
+        self.dx = moves[-1].dx
+        self.dy = moves[-1].dy
 
     def final_password(self):
-        p = 1000 * (self.y + 1) + 4 * (self.x + 1) + dirval(self.x, self.y)
+        p = 1000 * (self.y + 1) + 4 * (self.x + 1) + dirval[self.dx, self.dy]
         print("Final password: ", p)
-
-    def run(self):
-        for move in moves:
-            if self.x:
-                if self.row_free():
-                    xm = self.x * (abs(move[0])) % self.rowlen()
-                else:
-                    xm = s
-            self.apply_x(xm)
-
 
 def charcell(c):
     if c == ' ':
@@ -98,6 +141,8 @@ def get_moves(moveline):
             num = ''
             val = 0
 
+    moves.append(Move(dx, dy, val))
+
     return moves
 
 def get_gridmoves(lines):
@@ -115,19 +160,16 @@ def get_gridmoves(lines):
 
     return grid, moves
 
-
 def test():
     global grid, moves
     grid, moves = get_gridmoves(lines)
     m1 = get_moves('11L22L33L44LLL')
     m2 = get_moves('11R22R33R44RRR')
-    pass
+
+    nav = Nav(grid)
+    nav.run(moves)
+    nav.final_password()
 
 lines = util.readlinesf_ns('test_input')
 
 test()
-
-
-
-
-
